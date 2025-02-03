3b1b9d943290ae4cd92aec0ca5527d7121e300be
Mohammad Tahir <starghareeb@gmail.com>
2025-02-03 11:40:03 +0500

    most of the done...

--- src/components/Authentication/Login.jsx
+++ src/components/Authentication/Login.jsx
@@ -9,11 +9,8 @@ const Login = () => {
     const dispatch = useDispatch();
     const { isLoading, isError, errorMessage, user } = useSelector((state) => state.auth);
 
-    console.log(user);
-    
-    const handleChange = (e) => {
-        console.log(formData);
 
+    const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
 
@@ -28,7 +25,7 @@ const Login = () => {
         <>
             <section className="bg-gray-50 dark:bg-gray-900">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                 
+
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
